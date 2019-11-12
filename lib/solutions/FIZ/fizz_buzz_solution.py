3bca79aaebef1f26aeab2dd4c2db7ea46645614a
sbx_user1051 <sbx_user1051@169.254.30.253>
2019-11-12 15:11:04 +0000

    Tue Nov 12 15:11:04 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,23 +3,30 @@ def fizz_buzz(number):
     # raise NotImplementedError()
     n = number
     c_deluxe = ( len(set(str(n))) == 1 ) and ( n > 10 ) and ( n % 2 == 0)
-    c_deluxe_fake = ( len(set(str(n))) == 1 ) and ( n > 10 ) and ( n % 2 != 0)
+    c_fake_deluxe = ( len(set(str(n))) == 1 ) and ( n > 10 ) and ( n % 2 != 0)
     c_fizz = (( n % 3 ) == 0 ) or ('3' in str(n))
     c_buzz = (( n % 5 ) == 0 ) or ('5' in str(n))
     if c_fizz and c_buzz and c_deluxe:
         return "fizz buzz deluxe"
+    elif c_fizz and c_buzz and c_fake_deluxe:
+        return "fizz buzz fake deluxe"
     elif c_fizz and c_buzz:
         return "fizz buzz"
     elif c_fizz and c_deluxe:
         return "fizz deluxe"
     elif c_buzz and c_deluxe:
         return "buzz deluxe"
+    elif c_fizz and c_fake_deluxe:
+        return "fizz fake deluxe"
+    elif c_buzz and c_fake_deluxe:
+        return "buzz fake deluxe"
     elif c_fizz:
         return "fizz"
     elif c_buzz:
         return "buzz"
     elif c_deluxe:
         return "deluxe"
+    elif c_fake_deluxe:
+        return "fake deluxe"
     else:
         return n
-
