6a24846817f5b0330bde92ffb42224911710fbb3
sbx_user1051 <sbx_user1051@169.254.30.253>
2019-11-12 14:38:56 +0000

    Tue Nov 12 14:38:56 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,14 +2,14 @@
 def fizz_buzz(number):
     # raise NotImplementedError()
     n = number
-    if ((( n % 3 ) == 0 ) or ('3' in str(n))) \
+    if (( n % 3 ) == 0 ) or ('3' in str(n)) or \
+       (( n % 5 ) == 0 ) or ('5' in str(n)):
+        return "fizz buzz"
+    elif ((( n % 3 ) == 0 ) or ('3' in str(n))) \
         and ( ( n % 5 ) != 0 ) and ('5' not in str(n) ):
         return "fizz"
     elif (( n % 5 ) == 0 ) or ('5' in str(n)) \
         and (( n % 3 ) != 0 ) and ('3' not in str(n)):
         return "buzz"
-    elif (( n % 3 ) == 0 ) or ('3' in str(n)) or \
-       (( n % 5 ) == 0 ) or ('5' in str(n)):
-        return "fizz buzz"
     else:
         return n
