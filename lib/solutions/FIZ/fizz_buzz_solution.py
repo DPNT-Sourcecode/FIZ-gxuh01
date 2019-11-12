443958d203a4bcc4e2c62b6f9dbaa744720056d9
sbx_user1051 <sbx_user1051@169.254.30.253>
2019-11-12 15:27:58 +0000

    Tue Nov 12 15:27:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -7,7 +7,7 @@ def fizz_buzz(number):
     c3 = (( n % 3 ) == 0 ) and ('3' in str(n))
     c5 = (( n % 5 ) == 0 ) and ('5' in str(n))
     c_deluxe =  ( c3 or c5 ) and ( n % 2 == 0)
-    c_fake_deluxe = ( c3 and c5 ) and ( n % 2 != 0)
+    c_fake_deluxe = ( c3 or c5 ) and ( n % 2 != 0)
     c_fizz = (( n % 3 ) == 0 ) or ('3' in str(n))
     c_buzz = (( n % 5 ) == 0 ) or ('5' in str(n))
     if c_fizz and c_buzz and c_deluxe:
@@ -34,4 +34,5 @@ def fizz_buzz(number):
         return "fake deluxe"
     else:
         return n
+
 
