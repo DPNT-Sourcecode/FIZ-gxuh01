ccc9bc2492f56030148359747edc0dd7733ea635
sbx_user1051 <sbx_user1051@169.254.30.253>
2019-11-12 14:58:49 +0000

    Tue Nov 12 14:58:49 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,13 +2,22 @@
 def fizz_buzz(number):
     # raise NotImplementedError()
     n = number
+    c_deluxe = ( len(set(str(n))) == 1 )
     c_fizz = (( n % 3 ) == 0 ) or ('3' in str(n))
     c_buzz = (( n % 5 ) == 0 ) or ('5' in str(n))
-    if c_fizz and c_buzz:
+    if c_fizz and c_buzz and c_deluxe:
+        return "fizz buzz deluxe"
+    elif c_fizz and c_buzz:
         return "fizz buzz"
+    elif c_fizz and c_deluxe:
+        return "fizz deluxe"
+    elif c_buzz and c_deluxe:
+        return "buzz deluxe"
     elif c_fizz:
         return "fizz"
     elif c_buzz:
         return "buzz"
+    elif c_deluxe:
+        return "deluxe"
     else:
         return n
