85a8a841d197078bd011e6b0057ea28f8d911e81
sbx_user1051 <sbx_user1051@169.254.30.253>
2019-11-12 14:18:26 +0000

    Tue Nov 12 14:18:26 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    n = number
+    if (( n % 3 ) == 0 ) and (( n % 5 ) == 0 ):
+        print("fizz buzz")
+    elif (( n % 3 ) == 0 ):
+        print("fizz")
+    elif (( n % 5 ) == 0 ):
+        print("buzz")
+    else:
+        print(n)
